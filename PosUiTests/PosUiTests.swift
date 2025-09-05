b7f4477ab6684325d271b7f9113e8ccf93a8659f
chrisnewey <neweycm@gmail.com>
2025-09-05 09:10:29 -0600

    Initial Commit

---
+++ PosUiTests/PosUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  PosUiTests.swift
+//  PosUiTests
+//
+//  Created by Chris Newey on 9/5/25.
+//
+
+import Testing
+@testable import PosUi
+
+struct PosUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
